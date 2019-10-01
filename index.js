9c7c9078dd68910b855413845afb827346d6c0fe
Robert Diao <rdiao@student.unimelb.edu.au>
2019-10-01 21:28:22 +1000

    port env added

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var express = require('express');
 var app = express();
 
 const path = require('path');
+const port = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/docs/index.html'));
 });
 
-var server = app.listen(8000, function() {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
 

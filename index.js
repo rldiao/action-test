50c03509c3276471038ba6e7dc23fd7cb40ae6aa
Robert Diao <rdiao@student.unimelb.edu.au>
2019-10-01 21:00:26 +1000

    Index html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.sendFile("./docs/index.html");
 });
 
 var server = app.listen(8081, function() {
